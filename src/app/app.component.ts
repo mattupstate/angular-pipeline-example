a6adaeacb6cc2b701853e923b01e0a741876fd4b
Matt Wright <mwright@quartethealth.com>
2019-04-15 15:40:24 -0400

    Update the language selector to link to the current page intead of the root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, LOCALE_ID, Inject } from '@angular/core';
 import { NavigationAnalyticsService } from './analytics/navigation-analytics.service';
 import { PageTitleService } from './page-title.service';
 import { BuildInfo } from '../environments/build-info';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +12,27 @@ import { BuildInfo } from '../environments/build-info';
 })
 export class AppComponent {
   localeId = 'en-US';
+  currentUrl = '/';
   semVer = BuildInfo.semVer;
   gitCommit = BuildInfo.gitCommitHash;
 
   constructor(
+    @Inject(LOCALE_ID) localeId: string,
+    router: Router,
     pageTitle: PageTitleService,
-    navAnalaytics: NavigationAnalyticsService,
-    @Inject(LOCALE_ID) localeId: string
+    navAnalaytics: NavigationAnalyticsService
   ) {
     this.localeId = localeId;
     pageTitle.startTracking();
     navAnalaytics.startTracking();
+
+    router.events
+      .pipe(
+        filter(e => e instanceof NavigationEnd),
+        map((e: NavigationEnd) => e.urlAfterRedirects)
+      )
+      .subscribe((url: string) => {
+        this.currentUrl = url;
+      });
   }
 }
