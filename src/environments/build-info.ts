4718eefb3ac37e7b335d42ec4c48faa5b10b8e81
Matt Wright <mwright@quartethealth.com>
2019-04-15 13:16:57 -0400

    Use a different pattern for build-info.ts

---
+++ src/environments/build-info.ts
@@ -0,0 +1,10 @@
+/**
+ * This file is for placement only. A generated file will replace this file
+ * when the application is built for production.
+ */
+export class BuildInfo {
+  static semVer = 'null';
+  static gitCommitHash = 'null';
+  static gitBranch = 'null';
+  static isGitDirty = true;
+}
